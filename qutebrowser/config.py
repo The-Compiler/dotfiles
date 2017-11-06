88bc32b6a762a896ddd5d47097fd75f3afc6bbb1
Florian Bruhin <git@the-compiler.org>
2017-11-06 16:21:15 +0100

    Update qutebrowser config

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,6 +1,7 @@
 c.tabs.background = True
 c.new_instance_open_target = 'window'
 c.downloads.position = 'bottom'
+c.spellcheck.languages = ['en-US']
 
 config.bind(',ce', 'config-edit')
 config.bind(',rta', 'open {url}top/?sort=top&t=all')
@@ -8,3 +9,8 @@ config.bind(',p', 'config-cycle -p content.plugins ;; reload')
 
 css = '~/code/solarized-everything-css/css/solarized-all-sites-dark.css'
 config.bind(',n', f'config-cycle content.user_stylesheets {css} "" ;; reload')
+
+c.url.searchengines['rfc'] = 'https://tools.ietf.org/html/rfc{}'
+
+c.fonts.tabs = '8pt monospace'
+c.fonts.statusbar = '8pt monospace'
