702b4f7e52828dde7b550eb0042349c5ac8c2dea
Florian Bruhin <me@the-compiler.org>
2020-09-10 15:54:59 +0200

    doom: Add SPC p / binding

--- doom/config.el
+++ doom/config.el
@@ -72,6 +72,8 @@
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "C-=") (lambda () (interactive) (text-scale-set 0)))
+;; SPC /
+(map! :leader :desc "Search project" "p /" #'+default/search-project)
 
 ;; Never use pylint for flycheck (fails to load qute_pylint)
 (after! flycheck (setq-default flycheck-disabled-checkers '(python-pylint)))
