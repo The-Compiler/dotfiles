0f2baa83e2669ade20591f60939dcb44940b5af4
Florian Bruhin <me@the-compiler.org>
2021-05-17 16:27:06 +0200

    py: Add chunk()

--- startup.py
+++ startup.py
@@ -9,5 +9,9 @@ else:
     pretty.install()
     del pretty
 
+def chunk(elems, n):
+    for i in range(0, len(elems), n):
+        yield elems[i:i+n]
+
 sys.ps1 = '\001\033[96m\002>>> \001\033[0m\002'
 sys.ps2 = '\001\033[96m\002... \001\033[0m\002'
